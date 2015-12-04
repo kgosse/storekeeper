f303cf87deedb046a889a551ef6930cc7b72afa8
Kevin Gosse <gossekevin1@gmail.com>
2015-12-04 18:15:29 +0100

    initial commit - set up the dev environment

---
+++ src/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by kevin gosse on 04/12/2015.
+ */
+
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App.jsx';
+
+ReactDom.render(<App />, document.querySelector("#app"));
+
