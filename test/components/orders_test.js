bd2c5b908ba3271c223340c789c616809e188de0
Kevin Gosse <gossekevin1@gmail.com>
2015-12-06 14:18:41 +0100

    Unit Test Complex React Components

---
+++ test/components/orders_test.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import sinon from 'sinon';
+import expect from 'expect.js';
+import Orders from '../../src/components/Orders.jsx';
+import OrdersTable from '../../src/components/orders/orders_table.jsx';
+import { setupFakeDOM } from '../test_helper';
+
+setupFakeDOM();
+
+describe('components/orders', () => {
+  let subject;
+
+  beforeEach(() => {
+    subject = TestUtils.renderIntoDocument(<Orders />);
+  });
+
+  describe("#render", () => {
+    describe('when selectedStatus is "all"', () => {
+      beforeEach(() => {
+        subject.setState({ selectedStatus: 'all' });
+      });
+
+      it('renders all orders', () => {
+        const table = TestUtils.findRenderedComponentWithType(subject, OrdersTable);
+        const statuses = table.props.orders.map((o) => o.orderStatus);
+        expect( statuses ).to.eql([
+          'open', 'open', 'open', 'shipped'
+        ]);
+      });
+    });
+
+    describe('when selectedStatus is "open"', () => {
+      beforeEach(() => {
+        subject.setState({ selectedStatus: 'open' });
+      });
+
+      it('renders open orders', () => {
+        const table = TestUtils.findRenderedComponentWithType(subject, OrdersTable);
+        const statuses = table.props.orders.map((o) => o.orderStatus);
+        expect( statuses ).to.eql([
+          'open', 'open', 'open'
+        ]);
+      });
+    });
+
+    describe('when selectedStatus is "shipped"', () => {
+      beforeEach(() => {
+        subject.setState({ selectedStatus: 'shipped' });
+      });
+
+      it('renders shipped orders', () => {
+        const table = TestUtils.findRenderedComponentWithType(subject, OrdersTable);
+        const statuses = table.props.orders.map((o) => o.orderStatus);
+        expect( statuses ).to.eql([
+          'shipped'
+        ]);
+      });
+    });
+  });
+
+  describe("#handleStatusClick", () => {
+    it('calls setState with the new selectedStatus', () => {
+      sinon.spy( subject, 'setState' );
+      subject.handleStatusClick('foo');
+      expect( subject.setState.calledWith({ selectedStatus: 'foo' }) ).to.be( true );
+    });
+  });
+});
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,10 +1,16 @@
-import React from 'react/addons';
-const { TestUtils } = React.addons;
+import jsdom from 'jsdom';
+import TestUtils from 'react-addons-test-utils';
 
-function renderShallow(component){
+function setupFakeDOM() {
+    global.document = jsdom.jsdom("<!doctype html><html><body></body></html>");
+    global.window = document.defaultView;
+    global.navigator = { userAgent: "node.js" };
+}
+
+function renderShallow(component) {
     const renderer = TestUtils.createRenderer();
     renderer.render(component);
     return renderer.getRenderOutput();
 }
 
-export { renderShallow };
+export { renderShallow, setupFakeDOM };
